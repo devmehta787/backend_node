787dd05b82569fa16f5a463b0cc67b8e8f39bf8c
devmehta787 <devmehta787@gmail.com>
2024-05-12 01:08:52 +0530

    models for like, comment, playlist and post

---
+++ src/models/like.models.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+
+const likeSchema = new Schema({
+    video: {
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    },
+    Comment: {
+        type: Schema.Types.ObjectId,
+        ref: "Comment"
+    },
+    post: {
+        type: Schema.Types.ObjectId,
+        ref: "Post"
+    },
+    likedBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, { timestamps: true })
+
+export const Like = mongoose.model("Like", likeSchema)
