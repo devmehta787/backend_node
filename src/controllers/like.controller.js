c06512d0ad07ed91ab1fa0bbf855cffff7a37929
devmehta787 <devmehta787@gmail.com>
2024-05-19 01:48:41 +0530

    like controller methods

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -8,23 +8,147 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
-    //TODO: toggle like on video
+     if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video id")
+    }
+    
+    const liked = await Like.findOne({
+        video: videoId,
+        likedBy: req.user?._id
+    })
+    if (liked) {
+        await Like.findByIdAndDelete(liked._id)
+        
+        return res
+            .status(200)
+            .json(new ApiResponse(200, {liked: false}))
+    }
+
+    await Like.create({
+        video: videoId,
+        likedBy: req.user?._id
+    })
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, { liked: true }))
+    
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
-    //TODO: toggle like on comment
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid commentId");
+    }
+
+    const liked = await Like.findOne({
+        comment: commentId,
+        likedBy: req.user?._id,
+    })
+    if (liked) {
+        await Like.findByIdAndDelete(likedAlready?._id)
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, { isLiked: false }))
+    }
+
+    await Like.create({
+        comment: commentId,
+        likedBy: req.user?._id,
+    })
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, { isLiked: true }))
 
 })
 
 const togglePostLike = asyncHandler(async (req, res) => {
-    const {postId} = req.params
-    //TODO: toggle like on post
-}
-)
+    const { postId } = req.params
+    if(!isValidObjectId(postId)) {
+        throw new ApiError(400, "Invalid post id")
+    }
+
+    const liked = await Like.findOne({
+        post: postId,
+        likedBy: req.user?._id
+    })
+    if (liked) {
+        await Like.findByIdAndDelete(liked?._id)
+
+        return res
+            .status(200)
+            .json(new ApiResponse(200, { liked: false }))
+    }
+
+    await Like.create({
+        post: postId,
+        likedBy: req.user?._id
+    })
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, { isLiked: true }))
+})
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
+    const likedVideos = await Like.aggregate([
+        {
+            $match: {
+                likedBy: mongoose.Types.ObjectId(req.user?._id)
+            }
+        }, {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "likedVideos",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "ownerDetails"
+                        }
+                    }, {
+                        $unwind: "$ownerDetails"
+                    }
+                ]            
+            }
+        }, {
+            $unwind: "$likedVideos"
+        }, {
+            $sort: {
+              createdAt: -1  
+            }
+        }, {
+            $project: {
+                _id: 0,
+                likedVideos: {
+                    _id: 1,
+                    "videoFile.url": 1,
+                    "thumbnail.url": 1,
+                    owner: 1,
+                    title: 1,
+                    description: 1,
+                    description: 1,
+                    createdAt: 1,
+                    isPublished: 1,
+                    ownerDetails: {
+                        username: 1,
+                        fullName: 1,
+                        "avatar.url": 1
+                    }
+                }
+            }
+        }
+    ])
+
+    return res
+        .status(200)
+    .json(new ApiResponse(200, likedVideos, "Liked Videos fetched successfully"))
 })
 
 export {
